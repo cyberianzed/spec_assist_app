013ec102c1baa0a01106fdb14ee0d4ef9ccf995c
Jose K James <josekjames5@gmail.com>
2023-04-03 03:06:58 +0530

    n1

--- lib/ble/BackgroundCollectingTask.dart
+++ lib/ble/BackgroundCollectingTask.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:scoped_model/scoped_model.dart';
---
+++ lib/model/keys.dart
@@ -0,0 +1,3 @@
+
+
+const String API_KEY = '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';
